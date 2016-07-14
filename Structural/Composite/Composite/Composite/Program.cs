ded97b4558169e69906ed726ec7520863ac76254
LuizSirica <luizsirica@gmail.com>
2016-07-14 07:44:32 -0300

    Added Composite
    Patrick Motta Aragão Pissurno
    Luiz Roberto Almas Sirica
    Daniel Kropf

---
+++ Structural/Composite/Composite/Composite/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Patrick Motta Aragão Pissurno
+ * Luiz Roberto Almas Sirica
+ * Daniel Kropf
+*/
+
+namespace Composite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo ret = new Retangulo();
+            ret.Paint();
+            Console.ReadKey();
+        }
+    }
+    interface Graphic
+    {
+	    void Paint();
+	    void Add(Graphic g);
+	    void Remove(Graphic g);
+    }
+
+    //Objeto composto
+    class Retangulo : Graphic
+    {
+	    List<Graphic> components = new List<Graphic>();
+
+	    public Retangulo()
+        {
+		    for(int i=0; i<4; i++)
+		    {
+			    components.Add(new Linha());
+		    }
+	    }
+
+	    public void Paint()
+        {
+		    foreach(Graphic g in components)
+            {
+			    g.Paint();
+		    }
+	    }
+	    public void Add(Graphic g){ /*...*/ }
+	    public void Remove(Graphic g){ /*...*/ }
+    };
+
+    //Objeto simples
+    class Linha : Graphic
+    {
+	    public void Paint()
+        {
+            Console.WriteLine("███████████████");
+	    }
+	    public void Add(Graphic g){ /*...*/ }
+	    public void Remove(Graphic g){ /*...*/ }
+    }
+}

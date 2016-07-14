8bdc47b40ecff8d043d5cbc7f44410c91ae5f173
Patrick Pissurno <contato@patrickpissurno.com.br>
2016-07-14 11:01:22 -0300

    [Composite] Improved the code
    Refactored the 'Graphic' interface into 'IGraphic'

--- Structural/Composite/Composite/Composite/Program.cs
+++ Structural/Composite/Composite/Composite/Program.cs
@@ -15,50 +15,104 @@ namespace Composite
     {
         static void Main(string[] args)
         {
-            Retangulo ret = new Retangulo();
+            Retangulo ret = new Retangulo(ConsoleColor.Red);
+            Linha lin = new Linha(ConsoleColor.Green);
+            Ponto pon = new Ponto(ConsoleColor.Cyan);
+
+            Console.WriteLine("                            Demonstracao do Composite\n-------------------------------------------------------------------------------\n");
+
+            Console.WriteLine("Retangulo");
             ret.Paint();
+
+            Console.WriteLine("\nLinha");
+            lin.Paint();
+
+            Console.WriteLine("\nPonto");
+            pon.Paint();
+
+            Console.WriteLine("\n\n\n\n\n\n\n\n\n\n                         Aperte qualquer tecla para sair");
             Console.ReadKey();
         }
     }
-    interface Graphic
+    interface IGraphic
     {
 	    void Paint();
-	    void Add(Graphic g);
-	    void Remove(Graphic g);
+	    void Add(IGraphic g);
+	    void Remove(IGraphic g);
     }
 
     //Objeto composto
-    class Retangulo : Graphic
+    class Retangulo : IGraphic
     {
-	    List<Graphic> components = new List<Graphic>();
+	    private List<IGraphic> components = new List<IGraphic>();
 
-	    public Retangulo()
+	    public Retangulo(ConsoleColor color)
         {
 		    for(int i=0; i<4; i++)
 		    {
-			    components.Add(new Linha());
+                Add(new Linha(color));
 		    }
 	    }
 
 	    public void Paint()
         {
-		    foreach(Graphic g in components)
+		    foreach(IGraphic g in components)
             {
 			    g.Paint();
 		    }
 	    }
-	    public void Add(Graphic g){ /*...*/ }
-	    public void Remove(Graphic g){ /*...*/ }
+
+	    public void Add(IGraphic g)
+        {
+            components.Add(g);
+        }
+	    public void Remove(IGraphic g){ /*...*/ }
+    };
+
+    //Objeto composto
+    class Linha : IGraphic
+    {
+        private List<IGraphic> components = new List<IGraphic>();
+
+        public Linha(ConsoleColor color)
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                Add(new Ponto(color));
+            }
+        }
+
+        public void Paint()
+        {
+            foreach (IGraphic g in components)
+            {
+                g.Paint();
+            }
+            Console.WriteLine();
+        }
+
+        public void Add(IGraphic g)
+        {
+            components.Add(g);
+        }
+        public void Remove(IGraphic g) { /*...*/ }
     };
 
     //Objeto simples
-    class Linha : Graphic
+    class Ponto : IGraphic
     {
+        private ConsoleColor color;
+        public Ponto(ConsoleColor color)
+        {
+            this.color = color;
+        }
 	    public void Paint()
         {
-            Console.WriteLine("███████████████");
+            Console.ForegroundColor = color;
+            Console.Write("█");
+            Console.ResetColor();
 	    }
-	    public void Add(Graphic g){ /*...*/ }
-	    public void Remove(Graphic g){ /*...*/ }
+	    public void Add(IGraphic g){ /*...*/ }
+	    public void Remove(IGraphic g){ /*...*/ }
     }
 }
